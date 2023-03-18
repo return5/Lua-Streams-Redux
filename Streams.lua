8e7cc8f75abf07f3bccccb572e2ffdf44c74c03c
chris <armoredmoose@gmail.com>
2023-03-18 17:24:51 -0500

    added license info

--- Streams.lua
+++ Streams.lua
@@ -1,3 +1,21 @@
+--[[
+Program which aims to proved a lua module which mimics some of java 8 streams functionality.
+    Copyright (C) <2023>  <return5>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
 
 local setmetatable <const> = setmetatable
 local pairs <const> = pairs
